1787feb589cb6935cd8c5ffc48fdf3ef2a7eb785
Ray Jasson <48130098+rayjasson98@users.noreply.github.com>
2021-01-27 04:14:30 +0800

    Disable arrow key scrolling

--- js/main.js
+++ js/main.js
@@ -238,3 +238,14 @@ function setTextureImg() {
     });
   });
 }
+
+// Disable arrow key scrolling
+window.addEventListener(
+  "keydown",
+  e => {
+    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+    }
+  },
+  false
+);
